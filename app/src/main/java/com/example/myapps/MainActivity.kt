1a5a0ce8f9189357be617046affb23bbe92220f4
zidcraft879@gmail.com <zidcraft879@gamil.com>
2024-10-27 17:22:29 +0700

    add bottom navigation

--- app/src/main/java/com/example/myapps/MainActivity.kt
+++ app/src/main/java/com/example/myapps/MainActivity.kt
@@ -5,16 +5,36 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        replaceFragment(Product())
+
+        findViewById<BottomNavigationView>(R.id.bottom_navbar).setOnItemSelectedListener { item ->
+
+            when (item.itemId) {
+                R.id.product -> {
+                    replaceFragment(Product())
+                }
+
+                R.id.profile -> {
+                    replaceFragment(Profile())
+                }
+            }
+
+            return@setOnItemSelectedListener true
         }
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
